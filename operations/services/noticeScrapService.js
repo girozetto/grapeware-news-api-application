32bc9b630059d0cd7b8ac514e3de3fbee30eac81
girozetto <julbertoalexandredealmeida@gmail.com>
2023-12-02 10:38:49 +0100

    Corrigido os detalhes do scraping

--- operations/services/noticeScrapService.js
+++ operations/services/noticeScrapService.js
@@ -2,9 +2,10 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const dateConverter = require("../converters/dateConverters");
 const News = require("../models/news");
+const baseHealthyPageUrl = "https://minsa.gov.ao";
 
 const scrapeHealthyNews = async () => {
-  const pageUrl = "https://minsa.gov.ao/ao/noticias/";
+  const pageUrl = baseHealthyPageUrl + "/ao/noticias";
   const { data } = await axios.get(pageUrl);
   const $ = cheerio.load(data);
   let noticias = [];
@@ -15,13 +16,13 @@ const scrapeHealthyNews = async () => {
     const titulo = $(element).find("h2").text().trim();
     const textoData = $(element).find(".data").text().trim();
     const dataPublicacao = dateConverter.convertDate(textoData);
-    const link = pageUrl + $(element).find("a").attr("href");
+    const link = baseHealthyPageUrl + $(element).find("a").attr("href");
     const conteudoPreview = $(element).find("p").text().trim();
 
     const noticiaExistente = await News.getByTitle(titulo);
 
     if (!noticiaExistente && dataPublicacao > dataMaisRecenteNaBase) {
-      const { imageCoverUrl, content } = await scrapeNewsDetails(link);
+      const { imageCoverUrl, content } = await scrapeHealthyNewsDetails(link);
 
       noticias.push({
         titulo,
@@ -37,12 +38,12 @@ const scrapeHealthyNews = async () => {
   return noticias.sort((a, b) => a.dataPublicacao - b.dataPublicacao);
 };
 
-const scrapeNewsDetails = async (newsUrl) => {
+const scrapeHealthyNewsDetails = async (newsUrl) => {
   try {
     const { data } = await axios.get(newsUrl);
     const $ = cheerio.load(data);
 
-    const imageCoverUrl = $("div.cover img").attr("src");
+    const imageCoverUrl = baseHealthyPageUrl + $("div.cover img").attr("src");
     const content = $("div.container.small-container").text().trim();
 
     return { imageCoverUrl, content };
